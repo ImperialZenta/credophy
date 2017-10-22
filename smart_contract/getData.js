1f9df58be69a54f89e90463f190c92bdd7f872f0
Daniel Brodsky <c8z9a@ugrad.cs.ubc.ca>
2017-10-22 10:59:58 -0700

    updated

--- smart_contract/getData.js
+++ smart_contract/getData.js
@@ -2,23 +2,24 @@ var Web3 = require('web3');
 var TruffleContract = require("truffle-contract");
 var ABI = require("./build/contracts/Credophy.json");
 // module.exports.init = function () {
-var updateData = function(data){
-	var cert_contract = new TruffleContract(ABI);
-	var jdata = JSON.parse('{"id": ""}');
+// var updateData = function(data){
+	var Credophy = TruffleContract(ABI);
+	var jdata = JSON.parse('{"id": "00000001"}');
+	console.log(jdata["id"]);
 	// var keys = Object.keys(ABI["networks"]);
 	// var cert_address = ABI["networks"][keys[keys.length-1]]["address"];
 
 	var provider = new Web3.providers.HttpProvider("http://localhost:8545");
-	cert_contract.setProvider(provider);
+	Credophy.setProvider(provider);
 
 	var deployed;
-	cert_contract.deployed().then(function(instance) {
+	Credophy.deployed().then(function(instance) {
 		deployed = instance;
-		return deployed.getCert(jdata['id']);
+		return deployed.getCert(jdata["id"]);
 	}).then(function(result) {
 		return result;
 	}).catch(function(err) {
 		console.log(err);
 	  	return err;
 	});
-};
+// };
--- smart_contract/updateContract.js
+++ smart_contract/updateContract.js
@@ -2,23 +2,24 @@ var Web3 = require('web3');
 var TruffleContract = require("truffle-contract");
 var ABI = require("./build/contracts/Credophy.json");
 // module.exports.init = function () {
-var updateData = function(data){
-	var cert_contract = new TruffleContract(ABI);
-	var jdata = JSON.parse('{"id": ""}');
+// var updateData = function(data){
+	var Credophy = TruffleContract(ABI);
+	var jdata = JSON.parse('{"recipient": "0x3a8f101123c575a31287c5e6f1c568c7947ee0d4", "title": "idontcare", "_status": "idontcare", "uint": 1234, "_end_date": 1234}');
+	// console.log(jdata["id"]);
 	// var keys = Object.keys(ABI["networks"]);
 	// var cert_address = ABI["networks"][keys[keys.length-1]]["address"];
 
 	var provider = new Web3.providers.HttpProvider("http://localhost:8545");
-	cert_contract.setProvider(provider);
+	Credophy.setProvider(provider);
 
 	var deployed;
-	cert_contract.deployed().then(function(instance) {
+	Credophy.deployed().then(function(instance) {
 		deployed = instance;
-		return deployed.getCert(jdata['recipient'], jdata['title'], jdata['_status'], jdata['uint'], jdata['_end_date']);
+		return deployed.issueCert('0x3a8f101123c575a31287c5e6f1c568c7947ee0d4', jdata['title'], jdata['_status'], jdata['uint'], jdata['_end_date']);
 	}).then(function(result) {
 		return result;
 	}).catch(function(err) {
 		console.log(err);
 	  	return err;
 	});
-};
+// };
